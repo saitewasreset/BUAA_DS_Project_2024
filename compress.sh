36ea9ba92938e4866058d5251c0d5d80687f76b1
saitewasreset <saite@buaa.edu.cn>
2024-05-12 11:26:55 +0800

    Add compile.sh and compress.sh

---
+++ compress.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zip -r `date +%s`.zip main.c KeyInfoStream.c KeyInfoStream.h IdentifierHash.c IdentifierHash.h HashTableFast.c HashTableFast.h editdistDP.c CodeSimilarity.c CodeSimilarity.h
